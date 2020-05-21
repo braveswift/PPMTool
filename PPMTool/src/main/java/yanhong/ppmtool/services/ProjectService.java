f70797316cd07f7c15963ad60db66da0674d5a48
Yan Hong <hongyan968@gmail.com>
2020-05-20 22:04:08 -0700

    delete old project

--- PPMTool/src/main/java/yanhong/ppmtool/services/ProjectService.java
+++ PPMTool/src/main/java/yanhong/ppmtool/services/ProjectService.java
@@ -3,6 +3,7 @@ package yanhong.ppmtool.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import yanhong.ppmtool.domain.Project;
+import yanhong.ppmtool.exceptions.ProjectIdException;
 import yanhong.ppmtool.repositories.ProjectRepository;
 
 @Service
@@ -12,7 +13,14 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project) {
-        //Logic
-        return projectRepository.save(project);
+
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception e) {
+            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() +
+                    "' already exits.");
+        }
+
     }
 }
--- PPMTool/src/main/java/yanhong/ppmtool/web/ProjectController.java
+++ PPMTool/src/main/java/yanhong/ppmtool/web/ProjectController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,9 +13,6 @@ import yanhong.ppmtool.services.MapValidationErrorService;
 import yanhong.ppmtool.services.ProjectService;
 
 import javax.validation.Valid;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
