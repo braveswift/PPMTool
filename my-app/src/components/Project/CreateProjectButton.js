b0d7da912d10af30f34066300b1aa1bb58e47d90
Yan Hong <yanhong@MacBook-Pro.local>
2020-06-27 00:04:38 -0700

    complete update project

--- my-app/src/components/Project/CreateProjectButton.js
+++ my-app/src/components/Project/CreateProjectButton.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function CreateProjectButton() {
   return (
     <React.Fragment>
-      <Link to="/addProject" className="btn btn-lg btn-info">
+      <Link to="/addProject" className="btn btn-lg btn-info ml-3">
         Create a Project
       </Link>
     </React.Fragment>
